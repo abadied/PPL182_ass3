ceb6d2d9ab5743a3d8aca3217d0c2c3ce854082d
abadied <abadi.eden@gmail.com>
2018-05-15 18:49:19 +0300

    added tests to env and sub

--- hw3_part3_env/hw3-part3-tests.ts
+++ hw3_part3_env/hw3-part3-tests.ts
@@ -7,5 +7,46 @@ assert.deepEqual(evalParse4(`
     1);
 
 assert.deepEqual(evalParse4(`
+((lambda ((x lazy)) 1) (/ 1 0))`),
+    1);
+
+assert.deepEqual(evalParse4(`
+((lambda ((x lazy) (b lazy) c) c) (/ 1 0) (/ 1 0) 1)`),
+    1);
+
+assert.deepEqual(evalParse4(`
+((lambda ((x lazy) y) (if (= y 0) y x)) (/ 1 0) 0)`),
+    0);
+
+assert.deepEqual(evalParse4(`
+((lambda ((x lazy)) ((lambda ((x lazy)) 0) (/ 1 0))) (/ 1 0))`),
+0);
+
+assert.deepEqual(evalParse4(`
+(((lambda () (lambda ((x lazy)) 1))) (/ 1 0))`),
+1);
+
+assert.deepEqual(evalParse4(`(
+(lambda ((x lazy)) 1) (/ 1 0))`),
+1);
+
+assert.deepEqual(evalParse4(`
+(L4 (define factorial (lambda (fact)
+(if (= 0 fact) 1 (* fact (factorial (- fact 1)))))) (factorial 5))`),
+120);
+
+assert.deepEqual(evalParse4(`
+((lambda ((x lazy)) x) 5)`),
+5);
+
+assert.deepEqual(evalParse4(`
+((lambda ((x lazy)) ((lambda ((y lazy)) 5) x)) (/ 1 0))`),
+5);
+
+assert.deepEqual(evalParse4(`
+((lambda ((f lazy)) (f 1 1)) +)`),
+2);
+
+assert.deepEqual(evalParse4(`
     (L4 (if ((lambda ((x lazy)) (= x 10)) 10) #t #f))`),
         true);
--- hw3_part3_sub/hw3-part3-tests.ts
+++ hw3_part3_sub/hw3-part3-tests.ts
@@ -6,3 +6,20 @@ assert.deepEqual(evalParse(`
     ((lambda ((f lazy)) 1) (loop 0)))`),
     1);
 
+assert.deepEqual(evalParse(`
+(L3 (define loop (lambda ((a lazy)) 1) (/ 1 0)))`),
+    1);
+
+assert.deepEqual(evalParse(`
+(L3 ((lambda (y) (y (/ 1 0))) (lambda ((x lazy)) 1)))`),
+    1);
+
+assert.deepEqual(evalParse(`
+(L3 ((lambda ((x lazy) y) (y x)) (/ 1 0) (lambda ((x lazy)) 1))))`),
+    1);
+
+assert.deepEqual(evalParse(`
+(L3 ((lambda (x) (x (/ 1 0))) (lambda ((x lazy)) ((lambda ((x lazy)) 1) (/ 1 0))))))`),
+    1);
+
+    
